41dfb70621110b2cdcb27e83d23c286405c20a53
Jannchie <jannchie@gmail.com>
2023-07-28 04:19:34 +0900

    :sparkles: feat: touch

--- src/core/SheetWrapper.vue
+++ src/core/SheetWrapper.vue
@@ -21,8 +21,10 @@ const baseWrapperClass = computed(() => {
 
 const length = computed(() => data.length)
 const isPush = ref(false)
+const manualValue = ref(0)
 const currentValue = length
-const animateValue = useTransition(currentValue, {
+const temp = computed(() => manualValue.value ? manualValue.value : currentValue.value)
+const animateValue = useTransition(temp, {
   transition: TransitionPresets.easeOutExpo,
   duration: 500,
   onStarted: () => {
@@ -32,37 +34,42 @@ const animateValue = useTransition(currentValue, {
       isPush.value = false
   },
 })
+const trueValue = computed(() => {
+  return manualValue.value ? manualValue.value : animateValue.value
+})
 
 const sheetStyle = computed(() => {
   return (idx: number) => {
     if (isPush.value) {
       if (idx === currentValue.value - 1) {
         return {
-          transform: `translateY(${(1 + idx - animateValue.value) * 100}vh)`,
+          transform: `translateY(${(1 + idx - trueValue.value) * 100}vh)`,
         }
       }
       else {
         // start 'translateY(0) scale(1)'
         // end  'translateY(-3vh) scale(0.96)'
-        const progress = animateValue.value - idx - 1
+        const progress = trueValue.value - idx - 1
         return {
           transform: `translateY(${-3 * progress}vh) scale(${1 - progress * 0.04})`,
           filter: `brightness(${100 - 50 * progress}%)`,
         }
       }
     }
-    else {
+    else { // pop
       if (idx === currentValue.value) {
-        const progress = animateValue.value - idx
+        // start 'translateY(0) filter brightness(100%)'
+        // end  'translateY(-3vh) filter brightness(50%)'
+        const progress = trueValue.value - idx
         return {
-          transform: `translateY(${(1 + idx - animateValue.value) * 100}vh)`,
+          transform: `translateY(${(1 + idx - trueValue.value) * 100}vh)`,
           filter: `brightness(${50 + 50 * progress}%)`,
         }
       }
       else {
         // start 'translateY(-3vh) scale(0.96)'
         // end  'translateY(0) scale(1)'
-        const progress = animateValue.value - idx - 1
+        const progress = trueValue.value - idx - 1
         return {
           transform: `translateY(${-3 * progress}vh) scale(${1 - progress * 0.04})`,
           filter: `brightness(${100 - 50 * progress}%)`,
@@ -75,17 +82,19 @@ const sheetStyle = computed(() => {
 const onLeave: any = (el: HTMLElement, done: () => void) => {
   // requestAnimationFrame
   const idx = Number(el.dataset.index)
+  const currentTranslateY = el.style.transform.match(/translateY\((.*)vh\)/)?.[1]
+  console.log(currentTranslateY)
   const cb = () => {
-    if (animateValue.value === currentValue.value) {
+    if (trueValue.value === currentValue.value) {
       done()
       return
     }
     if (idx === currentValue.value) {
-      el.style.transform = `translateY(${(1 + idx - animateValue.value) * 100}vh)`
+      el.style.transform = `translateY(calc(${(1 + idx - trueValue.value) * 100}vh + ${currentTranslateY}vh))`
       el.style.filter = `brightness(${100}%)`
     }
     if (idx === currentValue.value - 1) {
-      const progress = animateValue.value - idx - 1
+      const progress = trueValue.value - idx - 1
       el.style.transform = `translateY(${-3 * progress}vh) scale(${1 - progress * 0.04})`
       el.style.filter = `brightness(${100 - 50 * progress}%)`
     }
@@ -94,15 +103,63 @@ const onLeave: any = (el: HTMLElement, done: () => void) => {
   requestAnimationFrame(cb)
 }
 
-const closeModal = useThrottleFn((e: PointerEvent, i: number) => {
-  if (i === currentValue.value - 2 && !(e.target as Element | null)?.classList.contains('ios-sheet-wrapper'))
-    return
-
+const throttlePop = useThrottleFn(() => {
   if (data.length > 1)
     data.pop()
 }, 500)
 
-const sheets = ref<HTMLElement | null>(null)
+const closeSheet = useThrottleFn((e: PointerEvent, i: number) => {
+  if (i === currentValue.value - 2 && !(e.target as Element | null)?.classList.contains('ios-sheet-wrapper'))
+    return
+  throttlePop()
+}, 500)
+
+const dragStartPos = ref({ x: 0, y: 0 })
+const progress = ref(0)
+const isDragging = ref(false)
+function onDragStart(e: PointerEvent) {
+  e.preventDefault()
+  isDragging.value = true
+  dragStartPos.value = { x: e.clientX, y: e.clientY }
+}
+
+let lastEventTime = window.performance.now()
+let lastEventY = 0
+let speedY = 0
+let timerId: number | null = null
+function onDrag(e: PointerEvent) {
+  e.preventDefault()
+  if (!isDragging.value)
+    return
+  const currentEventTime = window.performance.now()
+  const timeDifference = currentEventTime - lastEventTime
+  const dy = e.clientY - lastEventY
+  speedY = dy / timeDifference // 垂直速度，单位是像素/毫秒
+  // 更新上一个事件的时间和位置
+  lastEventTime = currentEventTime
+  lastEventY = e.clientY
+  const diff = e.clientY - dragStartPos.value.y
+  const height = window.innerHeight
+  progress.value = diff / height
+  // 清除上一个定时器
+  if (timerId !== null)
+    clearTimeout(timerId)
+
+  // 设置新的定时器
+  timerId = setTimeout(() => {
+    speedY = 0
+  }, 100) // 在100毫秒后如果没有新的pointermove事件，就将速度归零
+  if (progress.value < 0)
+    return
+  manualValue.value = -progress.value + currentValue.value
+}
+function onDragEnd(e: PointerEvent) {
+  e.preventDefault()
+  isDragging.value = false
+  if (speedY > 1)
+    throttlePop()
+  manualValue.value = 0
+}
 </script>
 
 <template>
@@ -116,11 +173,9 @@ const sheets = ref<HTMLElement | null>(null)
       <template v-for="(sheet, i) in data" :key="i">
         <div
           v-if="i > 0 && (i === currentValue - 2 || i === currentValue - 1)"
-          ref="sheets"
-          v-on-click-outside="(el) => closeModal(el, i)"
-          :data-index="i"
-          :style="sheetStyle(i)"
-          class="fixed inset-0 rounded-lg bg-neutral-900 top-[3vh]"
+          v-on-click-outside="(el) => closeSheet(el, i)" :data-index="i" :style="sheetStyle(i)"
+          class="fixed inset-0 rounded-lg bg-neutral-900 top-[3vh]" @pointerdown="onDragStart" @pointermove="onDrag"
+          @pointerup="onDragEnd"
         >
           <component :is="sheet" />
         </div>
